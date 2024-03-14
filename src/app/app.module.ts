184bfd02ea0a818454306ae6f2a57b43394651dd
MEHELLEB Yacine <archange34@hotmail.fr>
2024-03-14 02:29:19 +0100

    Envois des données tapées par l'étudiant en attendant leur récup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EnseignantComponent } from './enseignant/enseignant.component';
 import { EtudiantComponent } from './etudiant/etudiant.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EtudiantComponent } from './etudiant/etudiant.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/etudiant/etudiant.component.ts
+++ src/app/etudiant/etudiant.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-etudiant',
   templateUrl: './etudiant.component.html',
   styleUrls: ['./etudiant.component.css']
 })
-export class EtudiantComponent {
-
+export class EtudiantComponent implements OnInit {
+  /**J'utilise INPUT pour récupérer des données depuis mon composant
+   * parent qu'est ENSEIGNANT. Je dois le préciser dant le template aussi.
+   */
+  @Input() questionDeMonEnseignant!:string;
+  /** reponseEtudiant lit ce qui est tapé par l'étudiant */
+  reponseEtudiant!: string;
+  /**Je crée une variable où je déclare un EventEmitter via OUTPUT pour envoyer des données */
+  @Output() reponseFinale=new EventEmitter<string>();
+  /**j'envoies au parent ENSEIGNANT le contenu de reponseEtudiant */
+  envoyerReponse(){
+    this.reponseFinale.emit(this.reponseEtudiant);
+  }
+ngOnInit(): void {
+}
 }
