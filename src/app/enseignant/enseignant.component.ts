4ff377e54e66d5a6f492bd6ec370e7d1587ffc79
MEHELLEB Yacine <archange34@hotmail.fr>
2024-03-14 02:46:51 +0100

    Récup des données par le parent et correction

--- src/app/enseignant/enseignant.component.ts
+++ src/app/enseignant/enseignant.component.ts
@@ -9,10 +9,13 @@ export class EnseignantComponent implements OnInit{
   /**je commence par déclarer deux propriétés */
   question!: string;
   reponse!:string;
+  message!:string;
   ngOnInit(): void {
     /**je les initialise dans ngOnInit */
     this.question='Que font 2 X 2 ?';
     this.reponse='4';
   }
-
+  correction(r:string){
+      this.reponse===r?this.message='Bravo !':this.message='Euh...Faux !';
+  }
 }
