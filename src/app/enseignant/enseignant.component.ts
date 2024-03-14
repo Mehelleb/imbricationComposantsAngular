53deb46df7b3c57042a1214bf93261f8b8af7123
MEHELLEB Yacine <archange34@hotmail.fr>
2024-03-14 01:31:49 +0100

    affichage du composant ENSEIGANT dans le composant ROOT (app-component)

--- src/app/enseignant/enseignant.component.ts
+++ src/app/enseignant/enseignant.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-enseignant',
   templateUrl: './enseignant.component.html',
   styleUrls: ['./enseignant.component.css']
 })
-export class EnseignantComponent {
+export class EnseignantComponent implements OnInit{
+  /**je commence par déclarer deux propriétés */
+  question!: string;
+  reponse!:string;
+  ngOnInit(): void {
+    /**je les initialise dans ngOnInit */
+    this.question='Que font 2 X 2 ?';
+    this.reponse='4';
+  }
 
 }
